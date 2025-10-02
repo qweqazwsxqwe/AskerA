94db542c9d40067a0ba57d02496d93cb8ec48591
qweqazwsxqwe <3148222817@qq.com>
2025-10-02 15:40:30 +0800

    Update updater.py

--- run/updater.py
+++ run/updater.py
@@ -14,7 +14,7 @@ from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 # 配置参数
 REFRESH_INTERVAL = 2  # 刷新间隔（秒）
-TOTAL_REFRESH_COUNT = 100  # 总刷新次数
+TOTAL_REFRESH_COUNT = 60  # 总刷新次数
 WAIT_AFTER_TEST = 5  # 每次测速后等待时间（秒）
 
 # 基础User-Agent模板列表
